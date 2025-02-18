d9dcfb9822298fc627b7f704ed24c1986652c4cf
Raj Vamsee <vamsee2312@gmail.com>
2025-02-17 22:10:47 -0600

    Created and tested the base Chat Interface in VS Code Extensions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("initial setup of project for VS code extension"); 
+import * as vscode from 'vscode';
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('qbraid-chat.myChatCommand', () => {
+        vscode.window.showInformationMessage('Hey, qBraid-Chat is here to answer your queries!!');
+    });
+
+    context.subscriptions.push(disposable);
+}
+
+export function deactivate() { }
+
