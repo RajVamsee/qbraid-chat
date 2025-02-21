22bdf68748e4de58cf0ca63632a6c6cf94c3f62a
Raj Vamsee <vamsee2312@gmail.com>
2025-02-21 01:47:01 -0600

    Tried to verify setup before writing API logic

--- src/extension.ts
+++ src/extension.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
     let disposable = vscode.commands.registerCommand('qbraid-chat.myChatCommand', () => {
-        vscode.window.showInformationMessage('Hey!! qBraid can help you with your queries!!');
+        vscode.window.showInformationMessage('Hi guys, qBraid Chat will answer your queries !!');
     });
 
     context.subscriptions.push(disposable);
